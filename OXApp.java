a31a8314984516b414b4f61949d9cde459afa423
Yochki <71455712+Yochki@users.noreply.github.com>
2021-06-10 23:18:44 +0300

    Add files via upload

---
+++ OXApp.java
@@ -0,0 +1,19 @@
+package OXgame;
+
+import javax.swing.JFrame;
+
+public class OXApp {
+
+	public static void main(String[] args) {
+// TODO Auto-generated method stub
+		JFrame frame = new JFrame();
+		frame.setSize(400, 400);
+		frame.add(new OXPanel());
+		frame.setTitle("Naughts and crosses");
+		frame.setLocationRelativeTo(null);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+
+	}
+
+}
